6c4e748225f67fc2f333f116c25ed8bc52e3c264
Tech-Artemis <techartemis@outlook.com>
2023-08-20 05:27:20 +0000

    reduce&optimize

--- Chapter10/bulma/libs/bulma/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'bulma',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/bulma',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
--- Chapter11/ngx-behance/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  api: 'http://www.behance.net/v2/',
-  token: 'tIn2oeG0xiiUVQ5WsUsg9wWHOD1gjFuD'
-};
